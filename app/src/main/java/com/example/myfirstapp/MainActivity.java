7dd83e235dc495e0314f82cd29c5b7cdc9a95c3a
0429charlie <0429charlie@gmail.com>
2018-08-22 16:54:47 -0400

    First Try on ViewPager

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,19 +1,34 @@
 package com.example.myfirstapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
+    String pageData[];
+    LayoutInflater inflater;
+    ViewPager vp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pageData = getResources().getStringArray(R.array.desserts);
+        inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        vp = findViewById(R.id.viewPager);
+        vp.setAdapter(new MyPagesAdapter());
     }
 
     // Activity for button
@@ -23,5 +38,30 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    class MyPagesAdapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return pageData.length;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View page = inflater.inflate(R.layout.page, null);
+            ((TextView)page.findViewById(R.id.textMessage)).setText(pageData[position]);
+            container.addView(page,0);
+            return page;
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == arg1;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View)object);
+        }
     }
 }
